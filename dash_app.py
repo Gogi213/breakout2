472ca024256b64b36d4806337e98bf9b8198f422
Redmi <1993georgiy@gmail.com>
2023-12-26 18:12:40 +0200

    Разведение на plot

--- dash_app.py
+++ dash_app.py
@@ -4,22 +4,23 @@ from binance_api import get_top_futures_pairs, get_historical_futures_data
 import plot
 from analysis import find_pivot_high, find_pivot_low, find_pairs, find_low_pairs, validate_setup, validate_low_setup, find_breakout_candles
 
-# Создаем Dash-приложение
 app = dash.Dash(__name__)
 
-# Установите желаемый порог объёма
-volume_threshold = 150000000  # Например, 150 миллионов
-
-# Получаем список валютных пар
+volume_threshold = 150000000
 symbols = get_top_futures_pairs(volume_threshold=volume_threshold)
 
-# Определяем макет приложения
 app.layout = html.Div([
-    plot.create_layout_with_graph_and_list(symbols, symbols[0]),
-    html.Div(id='breakout-statistics-table')
+    dcc.Tabs([
+        dcc.Tab(label='Чарт', children=[
+            plot.create_layout_with_graph_and_list(symbols, symbols[0]),
+            html.Div(id='breakout-statistics-table')
+        ]),
+        dcc.Tab(label='Сводка таблиц', children=[
+            html.Div(id='content-tab-2')
+        ])
+    ])
 ])
 
-# Колбэк для обновления графика при выборе валютной пары
 @app.callback(
     [Output('currency-pair-graph', 'figure'),
      Output('breakout-statistics-table', 'children')],
